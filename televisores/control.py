b161a012e60c7c522f85d610aa65a49c2b247474
Johan Ramirez <159493340+joramirezma@users.noreply.github.com>
2024-02-26 14:32:44 -0500

    d

---
+++ televisores/control.py
@@ -0,0 +1,43 @@
+class Control:
+    def __init__(self,tv):
+        self.tv=tv
+
+    def turnOn(self):
+        self.tv.estado=True
+
+    def turnOff(self):
+        self.tv.estado=False
+
+    def canalUp(self):
+        if (self.tv.canal != 120 and self.tv.estado):
+            self.tv.canal += 1
+
+    def canalDown(self):
+        if (self.tv.canal != 1 and self.tv.estado):
+            self.tv.canal -= 1
+
+    def volumenUp(self):
+        if (self.tv.volumen != 7 and self.tv.estado):
+            self.tv.volumen += 1
+
+    def volumenDown(self):
+        if (self.tv.volumen != 0 and self.tv.estado):
+            self.tv.volumen -= 1
+    
+    def setCanal(self, canal):
+        if (1<=canal<=120 and self.tv.estado):
+            self.tv.canal = canal
+
+    def setVolumen(self, volumen):
+        if (0<=volumen<=7 and self.tv.estado):
+            self.tv.volumen=volumen
+    
+    def enlazar(self, tv):
+        self.tv=tv
+        tv.control=self
+    
+    def getTv(self):
+        return self.tv
+    
+    def setTv(self, tv):
+        self.tv=tv
--- televisores/tv.py
+++ televisores/tv.py
@@ -1,13 +1,3 @@
-class Marca:
-    def __init__(self, nombre):
-        self._nombre=nombre
-
-    def getNombre(self):
-        return self._nombre
-    
-    def setNombre(self, nombre):
-        self._nombre=nombre
-
 class TV:
     numTV=0
     def __init__(self, marca, estado):
@@ -81,48 +71,3 @@ class TV:
     def volumenDown(self):
         if (self.volumen != 0 and self.estado):
             self.volumen -= 1
-
-class Control:
-    def __init__(self,tv):
-        self.tv=tv
-
-    def turnOn(self):
-        self.tv.estado=True
-
-    def turnOff(self):
-        self.tv.estado=False
-
-    def canalUp(self):
-        if (self.tv.canal != 120 and self.tv.estado):
-            self.tv.canal += 1
-
-    def canalDown(self):
-        if (self.tv.canal != 1 and self.tv.estado):
-            self.tv.canal -= 1
-
-    def volumenUp(self):
-        if (self.tv.volumen != 7 and self.tv.estado):
-            self.tv.volumen += 1
-
-    def volumenDown(self):
-        if (self.tv.volumen != 0 and self.tv.estado):
-            self.tv.volumen -= 1
-    
-    def setCanal(self, canal):
-        if (1<=canal<=120 and self.tv.estado):
-            self.tv.canal = canal
-
-    def setVolumen(self, volumen):
-        if (0<=volumen<=7 and self.tv.estado):
-            self.tv.volumen=volumen
-    
-    def enlazar(self, tv):
-        self.tv=tv
-        tv.control=self
-    
-    def getTv(self):
-        return self.tv
-    
-    def setTv(self, tv):
-        self.tv=tv
-    
